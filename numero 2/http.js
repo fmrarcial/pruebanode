e558727190865074c8ca6085efa4b96534129145
brayanYU27 <gbrayanalberto@gmail.com>
2026-09-23 15:24:41 -0600

    cambioo

--- numero 2/http.js
+++ numero 2/http.js
@@ -1,29 +1,40 @@
-const http =require('node:http')
+const http = require('node:http')
+const fs = require('node:fs')
 
+const desiredPort = process.env.PORT ?? 1234
 
-const desiredPort = process.env.PORT ?? 1234 
-const server=http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.end('Bienvenido a mi primer servidor')
+    } else if (req.url === '/image.png') {
+        fs.readFile('imagenn.PNG', (err, data) => {
+            if (err) {
+                res.statusCode = 500
+                res.end('Error al leer la imakkgen')
+                return
+            }
 
-    if (req.url==='/'){
-    res.statusCode=200
-    res.setHeader('Content-Type','text/html;charset=utf-8')
-        res.end('bienvendido a mi primér servidor')
-} 
-else if (req.url==='/about'){
-    res.statusCode=200
-    res.setHeader('Content-Type','text/html;charset=utf-8')
-    res.end('<h1>Acerca de</h1><p>Este es un servidor HTTP básico en Node.js</p>')
-}
-else{
-    res.statusCode=404
-    res.setHeader('Content-Type','text/html;charset=utf-8')
-    res.end('<h1>404 Not Found</h1><p>La página que buscas no existe.</p>')   
-
-    
-}
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'image/png')
+            res.end(data)
+        })
+    } else if (req.url === '/about') {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.end(
+            '<h1>Acerca de</h1><p>Este es un servidor HTTP básico en Node.js</p>'
+        )
+    } else {
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.end(
+            '<h1>404 Not Found</h1><p>La página que buscas no existe.</p>'
+        )
+    }
 })
 
-
-    server.listen(desiredPort, () => {
-        console.log(`server listening on port http://localhost:${desiredPort}`)
-    })
+server.listen(desiredPort, () => {
+    console.log(`server listening on port http://localhost:${desiredPort}`)
+})
